4c8ea0c2510cce02320520cc73d1b1502666ad77
kokomade98 <c0305sh@naver.com>
2020-07-25 06:59:59 +0900

    err fix

--- api/c_store_list/get_detail_data.js
+++ api/c_store_list/get_detail_data.js
@@ -1,17 +1,39 @@
 // modules
 const response = require("../../utils/u_res");
+const sequelize = require('sequelize');
+const { Op } = sequelize;
 
 // models
 const models = require("../../models");
 const Store = models.Store;
 
 let get_detail_data = async (req, res) => {
-    let { start_lat, end_lat, start_lng, end_lng } = req.body;
-    console.log(start_lat, end_lat, start_lng, end_lng);
-    
+    let { start_lat, end_lat, start_lng, end_lng } = await req.body;
+    start_lat = parseFloat(start_lat);
+    // const data = await sequelize.query(sql, { replacements: values })
+    //     .spread(function (results, metadata) {
+    //         console.log('results: ', results);
+    //     }, function (err) {
+    //         console.log('err: ', err)
+    //     })
     let data = await Store.findAll({
-        where: { lat: { between: [start_lat, end_lat] }, lng: { between: [start_lng, end_lng] } }
+        where: {
+            lat: { [Op.gte]: start_lat, [Op.lte]: end_lat },
+            lng: { [Op.gte]: start_lng, [Op.lte]: end_lng },
+        },
     });
+    console.log('getData: ', data.length);
+
+    // console.log(start_lat, end_lat, start_lng, end_lng);
+
+    // let data = await Store.findAll({
+    //     // where: { lat: { gte: start_lat }, lat: { lte: end_lat }, lng: { gte: start_lng }, lng: { lte: end_lng }, }
+    //     where: {
+    //         lat: { [Op.between]: [37.5465729561758, 37.563662597033684] },
+    //         lng: { [Op.between]: [126.95718027928548, 126.9843350873578] }
+    //     }
+    // }
+    // );
 
     if (data) {
         response(res, 200, true, "[완료] 검색된 데이터의 정보를 반환합니다.", data);
